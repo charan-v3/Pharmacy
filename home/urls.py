f969528e9fde9000929abbea04171f483f22e180
Charan30092004 <charanvelumani@gmail.com>
2024-04-22 19:21:16 +0530

    created Medicine model

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-
-]
+    path("", views.home, name="home"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import medicine
+
+data = {"medicines": medicine.objects.all(), "title": "Home"}
 
 def home(request):
-    return render(request,'home/home.html')
+    return render(request, "home/home.html", data)
